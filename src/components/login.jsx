46531c125f58ea58d67dc299d6f380d032d75d2b
Vinokurov Sergey <Greendiezer@gmail.com>
2021-05-10 10:51:33 +0300

    feat(all components): second week homework: context, ref, proptypes
    Был создан контекст авторизации, изменена файловая система приложения, добавлена карта

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import PropTypes from "prop-types";
 
-export const Login = (props) => {
-
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        props.selectPage('map');
-    };
-
-        return (
-            
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Email:</label>
-                <input id="email" name="email" type="email"/>
-                <label htmlFor="password">Password:</label>
-                <input id="password" name="password" type="password"/>
-                <input type="submit" value="Войти" />
-            </form>
-        )
+Login.propTypes = {
+    toggleLoginForm: PropTypes.func,
+    handleLogin: PropTypes.func
 }
 
+export const Login = ({...props}) => {
+    return (
+        <form onSubmit={props.handleLogin}>
+            <TextField id="email" label="Standard" name="email" type="email"/>
+            <TextField
+                id="password"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+            />
+            <span>Новый пользователь? <span onClick={props.toggleLoginForm}>Регистрация</span></span>
+            <Button type="submit">Войти</Button>
+        </form>
+    ) 
+}
---
+++ src/pages/Map.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import mapboxgl from 'mapbox-gl';
+
+class Map extends Component {
+    map = null;
+    mapContainer = React.createRef();
+
+    componentDidMount() {
+        mapboxgl.accessToken = "pk.eyJ1IjoiZ3JlZW5kaWV6ZXIiLCJhIjoiY2tvZnNnbXB6MHBmeTJ2czkxYmoydGF2ZyJ9.4y8MsdOD5u8zx62cuqoHgA";
+
+        this.map = new mapboxgl.Map({
+            container: this.mapContainer.current,
+            style: "mapbox://styles/mapbox/streets-v9",
+            center: [30.3056504, 59.9429126],
+            zoom: 10
+        })
+    }
+
+    componentWillUnmount() {
+        this.map.remove();
+    }
+
+    render() { 
+        return ( 
+            <div className="map-wrapper">
+                <div data-testid="map" className="map" ref={this.mapContainer}></div>
+            </div>
+        );
+    }
+}
+
+export default Map;
